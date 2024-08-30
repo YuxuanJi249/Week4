15d49af052087c9b09489afd33738828593a2eb0
jyx <504468844@qq.com>
2024-08-30 10:49:15 +1000

    change

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,10 +6,14 @@ public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
     private int frameCount = 4;
+    public int i=200;
     public Renderer rend;
+    public int random;
     void Start()
-    {
-        
+    {    
+
+        random = Random.Range(150,251);
+        i= random;
     }
 
     // Update is called once per frame
@@ -18,9 +22,23 @@ public class PrintAndHide : MonoBehaviour
         Debug.Log($"{gameObject.name}:{frameCount}");
         frameCount++;
 
-        if ( frameCount > 6)
+        if (frameCount > 6)
         {
             rend.enabled = false;
         }
+
+        if (gameObject.CompareTag("Red"))
+        {
+            // If the tag is "Red" and i equals 100, deactivate the game object
+            if (i == 100)
+                gameObject.SetActive(false);
+        }
+        if (gameObject.CompareTag("Blue"))
+        {
+            if (i >= 150 && i <= 250)
+                rend.enabled = false;
+        }
+        i++;
     }
+
 }
